460402935ca89953c7f849861d675f3e3b9fd250
Jonathan <jon1969edwards@gmail.com>
2019-01-08 08:41:10 +0100

    Creates BMICalculator.js file and connects it to DisplayResult.js

--- src/DisplayResult.js
+++ src/DisplayResult.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { bmiCalculation } from './BMICalculator';
+
 
 class DisplayResult extends Component {
+  calculate() {
+    var weight= this.props.weight;
+    var height= this.props.height;
+
+    return bmiCalculation(weight, height);
+  }
 
   render() {
     return (
       <div id='response'>
+        {this.calculate()}
       </div>
     )
   }
